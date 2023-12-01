77e6ab783ff7b737eef634e814846710b1db879b
Jaiko657 <matthewboyd03@gmail.com>
2023-12-01 21:17:11 +0000

    Day 2: Trebuchet?! Part 2 Complete

--- AdventOfCode2023/day1.cpp
+++ AdventOfCode2023/day1.cpp
@@ -4,9 +4,33 @@
 
 using namespace std;
 
+int getNumber(char *ch) {
+    if (isdigit(*ch)) return (int)((*ch) - '0');
+    
+    if (strncmp(ch, "one", 3) == 0)
+        return 1;
+    else if (strncmp(ch, "two", 3) == 0)
+        return 2;
+    else if (strncmp(ch, "three", 5) == 0)
+        return 3;
+    else if (strncmp(ch, "four", 4) == 0)
+        return 4;
+    else if (strncmp(ch, "five", 4) == 0)
+        return 5;
+    else if (strncmp(ch, "six", 3) == 0)
+        return 6;
+    else if (strncmp(ch, "seven", 5) == 0)
+        return 7;
+    else if (strncmp(ch, "eight", 5) == 0)
+        return 8;
+    else if (strncmp(ch, "nine", 4) == 0)
+        return 9;
+    return -1;
+}
+
 int main() {
     std::ifstream file("input.txt"); // Replace with your file name
-    std::string line;   
+    std::string line;
 
     if (!file.is_open()) {
         std::cerr << "Unable to open file" << std::endl;
@@ -18,14 +42,16 @@ int main() {
         // Process each line as needed
         int currentNumber = 0;
         for (int i = 0; i < line.size(); i++) {
-            if (!isdigit(line[i])) continue;
-            currentNumber = line[i] - '0';
+            int num = getNumber(&line[i]);
+            if (num == -1) continue;
+            currentNumber = num;
             break;
         }
         currentNumber *= 10;
         for (int i = line.size()-1; i >= 0; i--) {
-            if (!isdigit(line[i])) continue;
-            currentNumber += line[i] - '0';
+            int num = getNumber(&line[i]);
+            if (num == -1) continue;
+            currentNumber += num;
             break;
         }
         sum += currentNumber;
