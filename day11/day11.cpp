2e8f4c1034949c822ae4c94a884aa5a8f694ca94
Jaiko657 <matthewboyd03@gmail.com>
2023-12-21 14:21:24 +0000

    Day 11: Cosmic Expansion Part 2 Complete

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -2,19 +2,50 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
+
+
 
 using namespace std;
 
+typedef unsigned long long ull;
+
 struct cord {
-	int x;
-	int y;
+	ull x;
+	ull y;
 };
 
-int getDistance(cord c1, cord c2) {
-    int xDistance = c1.x - c2.x;
-    int yDistance = c1.y - c2.y;
-    xDistance = abs(xDistance);
-    yDistance = abs(yDistance);
+ull getDistance(set<ull>* emptyCols, set<ull>* emptyRows, cord c1, cord c2) {
+    const ull EMPTY_WIDTH = 1000000;
+
+    // Calculate X distance
+    ull xDistance = 0;
+    ull startX = min(c1.x, c2.x);
+    ull endX = max(c1.x, c2.x);
+    ull finalX = startX;
+    for (ull x = startX; x < endX; x++) {
+        if (emptyCols->find(x) != emptyCols->end()) {
+            xDistance += EMPTY_WIDTH;
+        }
+        else {
+            xDistance++;
+            finalX = x;
+        }
+    }
+
+    // Calculate Y distance
+    ull yDistance = 0;
+    ull startY = min(c1.y, c2.y);
+    ull endY = max(c1.y, c2.y);
+    for (ull y = startY; y < endY; y++) {
+        if (emptyRows->find(y) != emptyRows->end()) {
+            yDistance += EMPTY_WIDTH;
+        }
+        else {
+            yDistance++;
+        }
+    }
+
     return xDistance + yDistance;
 }
 
@@ -27,56 +58,55 @@ int main() {
         file.close();
         return 1;
     }
-    //parse file into v
+    set<ull> emptyRows;
+    //parse file ullo v
     auto v = vector<vector<char>>();
     while (std::getline(file, line)) {
         // Process each line as needed
         auto vLine = vector<char>();
         bool isEmpty = true;
-        for (int i = 0; i < line.length(); i++) {
+        for (ull i = 0; i < line.length(); i++) {
             auto ch = line.at(i);
             if (ch != '.') isEmpty = false;
             vLine.push_back(ch);
         }
         if (isEmpty) {
-            //expand empty rows in v
-            v.push_back(vLine);
+            //find empty rows and add to set
+            //size should be current row being processed
+            emptyRows.insert(v.size());
         }
         v.push_back(vLine);
     }
     file.close();
-    //expand empty cols in v
-    for (int col = 0; col < v.at(0).size(); col++) {
+    //find empty cols and add to set
+    set<ull> emptyCols;
+    for (ull col = 0; col < v.at(0).size(); col++) {
         bool isEmpty = true;
-        for (int row = 0; row < v.size(); row++) {
+        for (ull row = 0; row < v.size(); row++) {
             auto c = v.at(row).at(col);
-            if (c != '.') isEmpty = false;
+            if (c == '#') isEmpty = false;
         }
         if (isEmpty) {
-            for (int row2 = 0; row2 < v.size(); row2++) {
-                auto* row = &v.at(row2);
-                row->insert(row->begin() + col, '.');
-            }
-            col++;
+            emptyCols.insert(col);
         }
     }
-    //get cords in v with '#'
+    //add galaxy cords to vector
     auto cords = vector<cord>();
-    for (int row = 0; row < v.size(); row++) {
-        for (int col = 0; col < v.at(0).size(); col++) {
+    for (ull row = 0; row < v.size(); row++) {
+        for (ull col = 0; col < v.at(0).size(); col++) {
             auto ch = v.at(row).at(col);
-            if (ch != '.') {
-                cord c = { row, col };
+            if (ch == '#') {
+                cord c = { col, row };
                 cords.push_back(c);
             }
         }
     }
 
     //find and sum distances
-    int sum = 0;
-    for (int i = 0; i < cords.size(); i++) {
-        for (int j = i + 1; j < cords.size(); j++) {
-            sum += getDistance(cords.at(i), cords.at(j));
+    ull sum = 0;
+    for (ull i = 0; i < cords.size(); i++) {
+        for (ull j = i + 1; j < cords.size(); j++) {
+            sum += getDistance(&emptyCols, &emptyRows, cords.at(i), cords.at(j));
         }
     }
     cout << sum << endl;
