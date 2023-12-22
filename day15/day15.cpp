ac85ccf13b6d917dde4834005f0f0fb25e565b27
Jaiko657 <matthewboyd03@gmail.com>
2023-12-22 02:19:20 +0000

    Day 15: Lens Library Part 2 Complete

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -1,40 +1,140 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Lens {
+    string label;
+    int focalLen;
+};
+int hashLabel(string label) {
+	int currentValue = 0;
+	for(const char ch : label) {
+		int asciiValue = static_cast<int>(ch);
+		currentValue += asciiValue;
+		currentValue *= 17;
+		currentValue %= 256;
+	}
+	return currentValue;
+}
+
+class Box {
+private:
+    static const int size = 256;
+public:
+    vector<Lens>* boxes[size];
+
+    Box() {
+        for (int i = 0; i < size; i++) {
+            boxes[i] = new vector<Lens>();
+        }
+    }
+    ~Box() {
+        for (int i = 0; i < size; i++) {
+            delete boxes[i];
+        }
+    }
+    void print() {
+		for (int i = 0; i < size; i++) {
+            auto box = boxes[i];
+            if (box->size() > 0) {
+                cout << "Box " << i << ": ";
+				for (const Lens lens : *box) {
+					cout << "[" << lens.label << " " << lens.focalLen << "] ";
+				}
+				cout << endl;
+            }
+		}
+    }
+    void removeLens(string label) {
+        int h = hashLabel(label);
+        auto box = boxes[h];
+        for(int i = 0; i < box->size(); i++) {
+            if (box->at(i).label == label) {
+                box->erase(box->begin() + i);
+            }
+        }
+    }
+    void addLens(string label, int focalLen) {
+        Lens lens = { label, focalLen };
+        int h = hashLabel(label);
+        auto box = boxes[h];
+        for (int i = 0; i < box->size(); i++) {
+            if (box->at(i).label == label) {
+                box->at(i).focalLen = focalLen;
+                return;
+            }
+        }
+        box->push_back(lens);
+    }
+    void processInstruction(const string& instruction) {
+        string label = "";
+        char type = ' ';
+        int focalLen = -1;
+        for (int i = 0; i < instruction.size(); i++) {
+            auto ch = instruction[i];
+            if (ch != '=' && ch != '-') {
+                label += ch;
+                continue;
+            }
+            type = ch;
+            if (type == '=') {
+                focalLen = (instruction[i + 1] - '0');
+            }
+            break;
+        }
+        // - type
+        if (focalLen == -1) {
+            removeLens(label);
+        }
+        // = type
+        else {
+            addLens(label, focalLen);
+        }
+    }
+    int getFocusingPower() {
+        int sum = 0;
+        for (int boxNumber = 0; boxNumber < size; boxNumber++) {
+            for (int slotNumber = 0; slotNumber < boxes[boxNumber]->size(); slotNumber++) {
+                int lensPower = (boxNumber + 1) * (slotNumber+1) * boxes[boxNumber]->at(slotNumber).focalLen;
+                sum += lensPower;
+            }
+        }
+        return sum;
+    }
+};
 int main() {
-    std::ifstream file("input.txt"); // Replace with your file name
-    std::string line;
+    ifstream file("input.txt"); // Replace with your file name
+    string line;
     if (!file.is_open()) {
-        std::cerr << "Unable to open file" << std::endl;
+        cerr << "Unable to open file" << endl;
         file.close();
         return 1;
     }
     getline(file, line);
+    Box b;
 
-    uint64_t sum = 0;
-    int currentValue = 0;
-    char* ch = &line[0];
-    while (true) {
-        if (*ch == ',' || *ch == '\0') {
-            cout << ": " << currentValue << endl;
-            sum += currentValue;
-            currentValue = 0;
-            if (*ch == '\0') {
-                break;
-            }
-            ch++;
-            continue;
+    //line = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    string currentSubstring;
+
+    for (char ch : line) {
+        if (ch != ',') {
+            currentSubstring += ch;
         }
-        cout << *ch;
-        int asciiValue = static_cast<int>(*ch);
-        currentValue += asciiValue;
-        currentValue *= 17;
-        currentValue %= 256;
-        ch++;
+        else {
+            if (!currentSubstring.empty()) {
+                b.processInstruction(currentSubstring);
+                currentSubstring.clear();
+            }
+        }
     }
-    cout << endl << sum << endl;
+    if (!currentSubstring.empty()) {
+        b.processInstruction(currentSubstring);
+    }
+
+    b.print();
+    cout << b.getFocusingPower() << endl;
     return 0;
 }
